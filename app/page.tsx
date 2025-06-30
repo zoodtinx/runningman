9906ec587e1b33955686345b6cd826798436ea2c
BangpleeBoi <peerapon.klajing@gmail.com>
2025-06-30 12:47:58 +0700

    chore:add fonts

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
    };
 
    return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
          <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
             <Image
                className="dark:invert"
@@ -19,10 +19,10 @@ export default function Home() {
                height={38}
                priority
             />
-            <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+            <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left ">
                <li className="mb-2 tracking-[-.01em]">
                   Get started by editing{" "}
-                  <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                  <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded  font-semibold">
                      app/page.tsx
                   </code>
                   .
@@ -31,8 +31,19 @@ export default function Home() {
                   Save and see your changes instantly.
                </li>
             </ol>
-
-            <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <div className=" text-[40px]">
+               <p className="font-light ">Text Texts</p>
+               <p className="font-medium ">Text Texts</p>
+               <p className="font-medium ">Text Texts</p>
+               <p className="font-semibold ">Text Texts</p>
+               <p className="font-bold ">Text Texts</p>
+               <p className="font-bold headline ">Headline</p>
+            </div>
+            <div className=" font-headline text-[40px]">
+               <p className=" headline ">HEADLINE</p>
+               <p className="font-bold headline ">HEADLINE</p>
+            </div>
+            <div className="flex gap-4 items-center flex-col sm:flex-row font-[family-name:var(--font-geist-sans)]">
                <a
                   className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
                   href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+   theme: {
+      extend: {
+         fontFamily: {
+            headline: ["var(--font-swiss-721-extended)", "sans-serif"],
+         },
+      },
+   },
+};
